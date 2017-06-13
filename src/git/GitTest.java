c813d87bbca3e602ee8ccc39d4e5e666556de71c
Administrator <Administrator@WQ-20160523IKRN>
2017-06-13 18:08:59 +0800

    哈哈

--- src/git/GitTest.java
+++ src/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("hello word£¬¹ş¹ş¹ş¹ş¹ş¹ş");
+         System.out.println("hello word£¬ÅèÅèÎûÎûÎûÎû");
 	}
 
 }
